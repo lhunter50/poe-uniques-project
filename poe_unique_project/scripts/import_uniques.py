b720a14de4abd003fc7ca487d92dd61006e025da
lhunter50 <luxstme@gmail.com>
2025-08-18 22:30:38 +0100

    set up initial models

--- poe_unique_project/scripts/import_uniques.py
+++ poe_unique_project/scripts/import_uniques.py
@@ -2,3 +2,8 @@ import os
 import django
 import requests
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "poe_unique_project.settings")
+django.setup()
+
+from uniques import BaseItem, UniqueItem
+
